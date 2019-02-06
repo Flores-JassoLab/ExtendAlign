a78ae9807db826e86ad8541eca6c7453a037c3a3
Israel Aguilar <iaguilaror@gmail.com>
2019-02-06 17:06:54 -0600

    changed input for module to .EAfa format

--- mkmodules/mk-create_blastdb/runmk.sh
+++ mkmodules/mk-create_blastdb/runmk.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ## This small script creates the targets for mk, then makes a request for each of them
-## It works with the following fasta file extensions: ".fa , .fna , .fasta"
+## It works with the EA fasta file extensions: ".EAfa"
 ## This files will be defined as the primary prereqs for mk
 ## The names of the final targets for mk will be defined by adding the extension: ".nsq"
   ## ^^since at the end of the makeblastdb process, *.nsq files are created
 find -L . \
   -type f \
-  -name "*.fa" -o -name "*.fna" -o -name "*.fasta" \
+  -name "*.EAfa" \
 | sed "s#\$#.nsq#" \
 | xargs mk $@
   ## ^^ "mk $@" enables to pass mk arguments from the main flow execution

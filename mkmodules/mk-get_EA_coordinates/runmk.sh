e50768c91d7658d4b2aa7ba7f84ed8b34b150f0b
Israel Aguilar <iaguilaror@gmail.com>
2019-02-10 19:07:56 -0600

    mk-get_EA_coordinates: implement homologation for outputs coming from besthit or allhits run

--- mkmodules/mk-get_EA_coordinates/runmk.sh
+++ mkmodules/mk-get_EA_coordinates/runmk.sh
@@ -3,12 +3,14 @@
 ## This small script creates the targets for mk, then makes a request for each of them
 ## It works with the following fasta file extensions: ".blastn.tsv" or ".blastnbesthit.tsv"
 ## This files will be defined as the primary prereqs for mk
-## The names of the final targets for mk will be defined by replacing the .tsv extension with: ".EAcoordinates.tsv"
+## The names of the final targets for mk will be defined by replacing the .blastn.tsv or .blastnbesthit.tsv extension with: ".EAcoordinates.tsv"
   ## ^^since at the end of the bed coordinates calculation process, *.EAcoordinates.tsv files are created
 find -L . \
   -type f \
   -name "*.blastn.tsv" \
   -o -name "*.blastnbesthit.tsv" \
-| sed "s#.tsv#.EAcoordinates.tsv#" \
+| sed \
+    -e "s#.blastn.tsv#.EAcoordinates.tsv#" \
+    -e "s#.blastnbesthit.tsv#.EAcoordinates.tsv#" \
 | xargs mk $@
   ## ^^ "mk $@" enables to pass mk arguments from the main flow execution
